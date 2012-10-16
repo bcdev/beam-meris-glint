3777cf38cb6ef23e3094de6f13986a5a1cb54d61
Olaf Danne <olaf.danne@brockmann-consult.de>
2012-10-16 15:26:56 +0200

    activated new atmospheric net '31x47x37_120805.4.net', provided by RD, 20121015

--- src/main/java/org/esa/beam/atmosphere/operator/GlintCorrectionOperator.java
+++ src/main/java/org/esa/beam/atmosphere/operator/GlintCorrectionOperator.java
@@ -92,7 +92,9 @@ public class GlintCorrectionOperator extends Operator {
     // another new net from RD, 2012/07/06:  (logrw_logrtosa)
 //    public static final String MERIS_ATMOSPHERIC_NET_NAME = "atmo_correct_meris/31x47x37_21434.7.net";
     // another new net from RD, 2012/06/28: more training!!!
-    public static final String MERIS_ATMOSPHERIC_NET_NAME = "atmo_correct_meris/31x47x37_29804.7.net";
+//    public static final String MERIS_ATMOSPHERIC_NET_NAME = "atmo_correct_meris/31x47x37_29804.7.net";
+    // another new net from RD, 2012/10/01: less "Rauschen" :-)
+    public static final String MERIS_ATMOSPHERIC_NET_NAME = "atmo_correct_meris/31x47x37_120805.4.net";
     // another new net from RD, 2012/06/28:
 //    public static final String MERIS_ATMOSPHERIC_NET_NAME = "atmo_correct_meris/31x47x37_57596.9.net";
     // another new net from RD, 2012/06/18:
